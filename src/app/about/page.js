9035bb33f203c7c041e403eaf6849578c1e0ccda
Kashyap J <kashyapj@wholesomeAir.local>
2025-06-12 14:29:30 +0530

    do i want to do this

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,4 +1,4 @@
-import prisma from '../../../lib/prisma';
+import prisma from '../../lib/prisma';
 import AboutPageClient from './AboutPageClient';
 
 export const revalidate = 7200; 
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { BlocksRenderer } from '@strapi/blocks-react-renderer';
 import Image from 'next/image'; // Import Next.js Image component
 import Link from 'next/link';   // Import Next.js Link component for navigation
 import WorkGrid from './components/WorkGrid';
-import prisma from '../../lib/prisma';
+import prisma from '../lib/prisma';
 
 export const revalidate = 60; // Optional: ISR
 
